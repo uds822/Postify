007a320c8e979c1338d5724f6b308b4ffcc8abaf
uds822 <140148878+uds822@users.noreply.github.com>
2024-11-13 00:01:17 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+// MongoDB Atlas connection string
+const uri = "mongodb+srv://postify:postify123@postify.4ifvc.mongodb.net/miniproject?retryWrites=true&w=majority";
 
+// Connect to MongoDB Atlas
+mongoose.connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB Atlas:", err);
+  });
+
+// Define the user schema
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
@@ -12,4 +23,5 @@ const userSchema = mongoose.Schema({
   posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }]
 });
 
+// Export the user model
 module.exports = mongoose.model('user', userSchema);
